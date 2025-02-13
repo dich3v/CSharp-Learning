e39b1ad17ccb0b9913172725fb336a81b3952d63
dich3v <radoslav.dichev@abv.bg>
2025-02-13 16:16:11 +0200

    Task added

---
+++ CS Exercises/Task_Actor_Nominee_Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_Actor_Nominee_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double startPoints = double.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            double finalPoints = startPoints;
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double points = double.Parse(Console.ReadLine());
+
+                finalPoints += (name.Length * points) / 2;
+                if (finalPoints > 1250.5)
+                {
+                    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {finalPoints:f1}!");
+                    return;
+                }
+            }
+            Console.WriteLine($"Sorry, {actorName} you need {1250.5 - finalPoints:f1} more!");
+        }
+    }
+}
--- CS Exercises/Task_Histogram/obj/Debug/net6.0/Task_Histogram.AssemblyInfo.cs
+++ CS Exercises/Task_Histogram/obj/Debug/net6.0/Task_Histogram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_Histogram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bfc19db4083a01f7e8879fca6d9080b4e1ec5ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c214477318f045d9dd16f771421a802956d73ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_Histogram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_Histogram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
