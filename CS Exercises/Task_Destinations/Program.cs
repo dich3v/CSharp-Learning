8a3bbdd14fcaf22de3f5a2b525d69b85d82ecc27
dich3v <radoslav.dichev@abv.bg>
2025-02-21 18:24:44 +0200

    Collecting money for given destination

---
+++ CS Exercises/Task_Destinations/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_Destinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while (destination != "End")
+            {
+                double collectedMoney = 0;
+                double neededBudget = double.Parse(Console.ReadLine());
+                while (neededBudget > collectedMoney)
+                {
+                    collectedMoney += double.Parse(Console.ReadLine());
+                    Console.WriteLine($"Collected: {collectedMoney:F2}");
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
