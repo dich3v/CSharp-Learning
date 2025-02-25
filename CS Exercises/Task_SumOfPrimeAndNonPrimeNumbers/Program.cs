87d650a4a12e8d5397a36ba38a3ab6a405d8b4de
dich3v <radoslav.dichev@abv.bg>
2025-02-25 17:10:48 +0200

    Finding the sum of all prime and all non-prime numbers

---
+++ CS Exercises/Task_SumOfPrimeAndNonPrimeNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Task_SumOfPrimeAndNonPrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int sumOfPrimeNumbers = 0;
+            int sumOfNonPrimeNumbers = 0;
+
+            while (input != "stop")
+            {
+                int number = int.Parse(input);
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                }
+                else if (number == 0 || number == 1)
+                {
+                    sumOfNonPrimeNumbers += number;
+                }
+                else
+                {
+                    bool isPrime = true;
+                    for (int i = 2; i <= Math.Sqrt(number); i++)
+                    {
+                        if (number % i == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        sumOfPrimeNumbers += number;
+                    }
+                    else
+                    {
+                        sumOfNonPrimeNumbers += number;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {sumOfPrimeNumbers}");
+            Console.WriteLine($"Sum of all non prime numbers is: {sumOfNonPrimeNumbers}");
+        }
+    }
+}
