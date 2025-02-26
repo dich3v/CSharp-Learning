c0bbcf8fb4894383b55a5999ebd3a253e64d7bb6
dich3v <radoslav.dichev@abv.bg>
2025-02-26 16:24:53 +0200

    Printing triangle

---
+++ CS Exercises/Task_Printing_Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintTriangle(n);
+        }
+        static void PrintTriangle(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = num - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    }
+}
