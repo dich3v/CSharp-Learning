4df7e9878e28d65a4d6f1e00269127e48eed714f
dich3v <radoslav.dichev@abv.bg>
2025-02-26 16:18:16 +0200

    Calculates whether the budget is enough for a holiday

---
+++ CS Exercises/Task_BudgetForHoliday/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_BudgetForHoliday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int nights = int.Parse(Console.ReadLine());
+            double pricePerNight = double.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            if (nights > 7)
+            {
+                pricePerNight *= 0.95;
+            }
+            double moneyNeeded = (nights * pricePerNight) + (budget * (percent / 100));
+
+            if (moneyNeeded <= budget)
+            {
+                Console.WriteLine($"Ivanovi will be left with {budget - moneyNeeded:f2} leva after vacation.");
+            }
+            else
+            {
+                Console.WriteLine($"{moneyNeeded - budget:f2} leva needed.");
+            }
+        }
+    }
+}
