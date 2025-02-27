3def279aa1e102d87faf29e6e2aad6ca519a1158
dich3v <radoslav.dichev@abv.bg>
2025-02-27 17:59:50 +0200

    Method to repeat text 'n' times.

---
+++ CS Exercises/Task_RepeatingText/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Task_RepeatingText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string result = RepeatedText(input, n);
+
+            Console.WriteLine(result);
+        }
+        static string RepeatedText(string text, int repeat)
+        {
+            StringBuilder repeatedText = new StringBuilder();
+            for (int i = 0; i < repeat; i++)
+            {
+                repeatedText.Append(text);
+            }
+            return repeatedText.ToString();
+        }
+    }
+}
