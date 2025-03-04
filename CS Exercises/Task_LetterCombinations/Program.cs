6ee4e5ad5d677eaf36214b3647a20a54a603e755
dich3v <radoslav.dichev@abv.bg>
2025-03-04 13:54:03 +0200

    Exclude specific letter in char combinations

---
+++ CS Exercises/Task_LetterCombinations/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_LetterCombinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char thirdLetter = char.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (char firstChar = firstLetter; firstChar <= secondLetter; firstChar++)
+            {
+                for (char secondChar = firstLetter; secondChar <= secondLetter; secondChar++)
+                {
+                    for (char thirdChar = firstLetter; thirdChar <= secondLetter; thirdChar++)
+                    {
+                        if (firstChar != thirdLetter && secondChar != thirdLetter && thirdChar != thirdLetter)
+                        {
+                            Console.Write($"{firstChar}{secondChar}{thirdChar} ");
+                            count++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"\n{count}");
+        }
+    }
+}
