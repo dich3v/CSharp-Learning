fbb08390711395a91e105e6ddc40d901a37ad61a
dich3v <radoslav.dichev@abv.bg>
2025-03-11 18:59:22 +0100

    Implemented factorial sum calculation for even digits of a number.

---
+++ CS Exercises/Task_SumFactorialsOfEvenDigits/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_SumFactorialsOfEvenDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int factorialsSum = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                if (digit % 2 == 0)
+                {
+                    int result = 1;
+
+                    for (int i = 2; i <= digit; i++)
+                    {
+                        result *= i;
+                    }
+                    factorialsSum += result;
+                }
+                n /= 10;
+            }
+            Console.WriteLine(factorialsSum);
+        }
+    }
+}
