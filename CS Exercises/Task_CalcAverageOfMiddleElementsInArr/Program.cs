526b515e17daf42c1105ecb897023e99e579bd26
dich3v <radoslav.dichev@abv.bg>
2025-03-11 19:02:20 +0100

    Implemented middle element(s) average calculation for an array.

---
+++ CS Exercises/Task_CalcAverageOfMiddleElementsInArr/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_CalcAverageOfMiddleElementsInArr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            double middleSum = 0;
+
+            if (arr.Length % 2 != 0)
+            {
+                int middleIndex = arr.Length / 2;
+                middleSum = arr[middleIndex];
+            }
+            else
+            {
+                int middleIndex1 = (arr.Length / 2) - 1;
+                int middleIndex2 = arr.Length / 2;
+
+                middleSum = (arr[middleIndex1] + arr[middleIndex2]) / 2.0;
+            }
+
+            Console.WriteLine($"{middleSum:F2}");
+        }
+    }
+}
