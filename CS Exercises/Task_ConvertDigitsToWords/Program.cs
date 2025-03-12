1f45f573954fb1f0284bf55cf07a50656ccf9965
dich3v <radoslav.dichev@abv.bg>
2025-03-12 16:52:25 +0100

    Implement logic to print each digit of a number as a word

---
+++ CS Exercises/Task_ConvertDigitsToWords/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_ConvertDigitsToWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            while (n > 0)
+            {
+                int digit = n % 10;
+                string digitString = digit switch
+                {
+                    1 => "one",
+                    2 => "two",
+                    3 => "three",
+                    4 => "four",
+                    5 => "five",
+                    6 => "six",
+                    7 => "seven",
+                    8 => "eight",
+                    9 => "nine",
+                    _ => "zero"
+                };
+                Console.WriteLine(digitString);
+                n /= 10;
+            }
+        }
+    }
+}
