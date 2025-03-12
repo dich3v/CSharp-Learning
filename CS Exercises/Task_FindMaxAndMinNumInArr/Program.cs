b149c9d8cbca2e6de64c1f6bee73d285b54397e0
dich3v <radoslav.dichev@abv.bg>
2025-03-12 16:49:50 +0100

    Finding max and min values in the elements of an array

---
+++ CS Exercises/Task_FindMaxAndMinNumInArr/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_FindMaxAndMinNumInArr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (numbers[i] > maxNum)
+                {
+                    maxNum = numbers[i];
+                }
+                if (numbers[i] < minNum)
+                {
+                    minNum = numbers[i];
+                }
+            }
+            Console.WriteLine(maxNum);
+            Console.WriteLine(minNum);
+        }
+    }
+}
