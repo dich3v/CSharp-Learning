ecbbc058e49aa785df5448a7a49cd9cba702a6f3
dich3v <radoslav.dichev@abv.bg>
2025-03-13 17:36:30 +0100

    Sort the products list and print each product with its index

---
+++ CS Exercises/Task_SortAndListProducts/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_SortAndListProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+            products.Sort();
+
+            int count = 1;
+            foreach (string product in products)
+            {
+                Console.WriteLine($"{count}.{product}");
+                count++;
+            }
+        }
+    }
+}
