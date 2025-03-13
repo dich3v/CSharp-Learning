f97b07f8cf534dc6ca0dd284cb07fb37cf28efda
dich3v <radoslav.dichev@abv.bg>
2025-03-13 17:34:06 +0100

    Implement Delete and Insert operations on a list based on user input

---
+++ CS Exercises/Task_SimpleListCommands/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_SimpleListCommands
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            string command = "";
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] input = command.Split(" ");
+
+                if (input[0] == "Delete")
+                {
+                    while (numbers.Contains(int.Parse(input[1])))
+                    {
+                        numbers.Remove(int.Parse(input[1]));
+                    }
+                }
+                else if (input[0] == "Insert")
+                {
+                    int value = int.Parse(input[1]);
+                    int index = int.Parse(input[2]);
+
+                    numbers.Insert(index, value);
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
