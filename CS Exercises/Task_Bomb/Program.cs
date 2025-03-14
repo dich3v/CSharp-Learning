d4f4c1f681ffd73d25159cf248cea79435c09a02
dich3v <radoslav.dichev@abv.bg>
2025-03-14 17:34:24 +0100

    Remove elements around bomb positions and calculate the sum

---
+++ CS Exercises/Task_Bomb/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_Bomb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int[] bombNums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int bomb = bombNums[0];
+            int bombPower = bombNums[1];
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == bomb)
+                {
+                    int startIndex = i - bombPower;
+                    int endIndex = i + bombPower;
+
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+                    if (endIndex >= numbers.Count)
+                    {
+                        endIndex = numbers.Count - 1;
+                    }
+                    numbers.RemoveRange(startIndex, endIndex - startIndex + 1);
+                    i = startIndex - 1;
+                }
+            }
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
