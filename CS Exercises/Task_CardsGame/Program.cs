a68b7ad97efff8e56e5aabd18b3ad93014c88c89
dich3v <radoslav.dichev@abv.bg>
2025-03-14 17:24:51 +0100

    Simulate card game between two players

---
+++ CS Exercises/Task_CardsGame/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_CardsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstPlayer = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> secondPlayer = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            while (firstPlayer.Count > 0 && secondPlayer.Count > 0)
+            {
+                int firstPlayerCard = firstPlayer[0];
+                int secondPlayerCard = secondPlayer[0];
+
+                if (firstPlayerCard > secondPlayerCard)
+                {
+                    firstPlayer.Add(firstPlayerCard);
+                    firstPlayer.Add(secondPlayerCard);
+                }
+                else if (secondPlayerCard > firstPlayerCard)
+                {
+                    secondPlayer.Add(secondPlayerCard);
+                    secondPlayer.Add(firstPlayerCard);
+                }
+                firstPlayer.RemoveAt(0);
+                secondPlayer.RemoveAt(0);
+            }
+            string winner = firstPlayer.Count > secondPlayer.Count ? "First" : "Second";
+            int winnerSum = firstPlayer.Count > secondPlayer.Count ? firstPlayer.Sum() : secondPlayer.Sum();
+            Console.WriteLine($"{winner} player wins! Sum: {winnerSum}");
+        }                      
+    }
+}
