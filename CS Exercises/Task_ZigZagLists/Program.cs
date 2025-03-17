5495566647da4a5e3d9f75598f4b5a670fb74d90
dich3v <radoslav.dichev@abv.bg>
2025-03-17 22:32:38 +0200

    Implement Zig-Zag List population based on alternating input order

---
+++ CS Exercises/Task_ZigZagLists/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_ZigZagLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = new List<int>();
+            List<int> secondList = new List<int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                int first = input[0];
+                int second = input[1];
+
+                if (i % 2 == 0)
+                {
+                    firstList.Add(first);
+                    secondList.Add(second);
+                }
+                else
+                {
+                    firstList.Add(second);
+                    secondList.Add(first);
+                }
+            }
+            Console.WriteLine(string.Join(" ", firstList));
+            Console.WriteLine(string.Join(" ", secondList));
+        }
+    }
+}
