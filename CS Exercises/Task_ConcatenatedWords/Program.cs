4fc2e44d6d72b5276c3f9dec0a3e479031e13518
dich3v <radoslav.dichev@abv.bg>
2025-03-20 15:59:45 +0200

    Implemented word concatenation based on length

---
+++ CS Exercises/Task_ConcatenatedWords/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Task_ConcatenatedWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+
+            StringBuilder concantenatedString = new StringBuilder();
+
+            foreach (string word in input)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    concantenatedString.Append(word);
+                }
+            }
+            Console.WriteLine(concantenatedString);
+        }
+    }
+}
--- CS Exercises/Task_TextFilter/obj/Debug/net6.0/Task_TextFilter.AssemblyInfo.cs
+++ CS Exercises/Task_TextFilter/obj/Debug/net6.0/Task_TextFilter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_TextFilter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de20c301763be9971833cd0fd122beafa32dc6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9399fb5b5d46072d9f555f57132d6bd6e3e3da75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_TextFilter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_TextFilter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
