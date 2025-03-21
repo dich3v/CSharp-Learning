293c2ce3700e38c25b538b0617fcb4940282d8a9
dich3v <radoslav.dichev@abv.bg>
2025-03-21 16:16:45 +0200

    Remove all occurrences of a word from a given text

--- CS Exercises/Task_MatchDates/obj/Debug/net6.0/Task_MatchDates.AssemblyInfo.cs
+++ CS Exercises/Task_MatchDates/obj/Debug/net6.0/Task_MatchDates.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_MatchDates")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed33a430d39dcf67efe3bafcd772962750af53c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd594e9e1075b60ceaeffce64851c02b565d749")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_MatchDates")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_MatchDates")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CS Exercises/Task_RemoveWordFromText/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_RemoveWordFromText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(word))
+            {
+                int index = text.IndexOf(word);
+                int count = word.Length;
+                text = text.Remove(index, count);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
