45777918e92c285ad3fe5568fe6af5a33c3eb080
dich3v <radoslav.dichev@abv.bg>
2025-04-02 09:12:33 +0300

    Add input validation and range checks for numbers

---
+++ CS Exercises/Task_NumbersInRange/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task_NumbersInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum = 1;
+            int endNum = 100;
+
+            ReadNumber(startNum, endNum);
+        }
+        public static void ReadNumber(int start, int end)
+        {
+            List<int> numbers = new();
+
+            while (numbers.Count < 10)
+            {
+                try
+                {
+                    int num = int.Parse(Console.ReadLine());
+
+                    if (num > start && num < end)
+                    {
+                        numbers.Add(num);
+                        start = num;
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Your number is not in range {start} - 100!");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
