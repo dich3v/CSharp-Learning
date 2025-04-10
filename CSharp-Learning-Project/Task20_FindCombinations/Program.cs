0ff61b5f8b99cd2ba35c00de6561668bb3848581
dich3v <radoslav.dichev@abv.bg>
2025-04-10 10:45:46 +0300

    Find four-digit combinations summing to number

---
+++ CSharp-Learning-Project/Task20_FindCombinations/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task20_FindCombinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int k = 0; k <= n; k++)
+                    {
+                        for (int l = 0; l <= n; l++)
+                        {
+                            int a = i + j;
+                            int b = k + l;
+
+                            if (a == n && b == n && a == b)
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
