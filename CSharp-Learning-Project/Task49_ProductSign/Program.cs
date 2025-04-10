1764ddbf657e4adc9307157ba53581228c32b0a6
dich3v <radoslav.dichev@abv.bg>
2025-04-10 12:39:52 +0300

    Added sign-checking function for three integers

---
+++ CSharp-Learning-Project/Task49_ProductSign/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task49_ProductSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            string result = PrintSign(num1, num2, num3);
+
+            Console.WriteLine(result);
+        }
+        static string PrintSign(int num1, int num2, int num3)
+        {
+            if (num1 == 0 || num2 == 0 || num3 == 0)
+            {
+                return "zero";
+            }
+            int negativeCount = 0;
+
+            if (num1 < 0)
+            {
+                negativeCount++;
+            }
+            if (num2 < 0)
+            {
+                negativeCount++;
+            }
+            if (num3 < 0)
+            {
+                negativeCount++;
+            }
+            return (negativeCount % 2 == 1) ? "negative" : "positive";
+        }
+    }
+}
