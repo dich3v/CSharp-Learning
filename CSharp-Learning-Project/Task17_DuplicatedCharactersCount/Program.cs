3e3ed3692567c929595da6e34cdfaff53ef56dc7
dich3v <radoslav.dichev@abv.bg>
2025-04-10 10:31:52 +0300

    Added character frequency counter using HashSet and StringBuilder

---
+++ CSharp-Learning-Project/Task17_DuplicatedCharactersCount/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task17_DuplicatedCharactersCount
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = GetCharacterCounts(input);
+            Console.WriteLine(result);
+        }
+
+        public static string GetCharacterCounts(string text)
+        {
+            text = text.ToLower();
+            HashSet<char> set = new HashSet<char>(text);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in set)
+            {
+                int count = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (c == text[i])
+                    {
+                        count++;
+                    }
+                }
+                sb.Append(c);
+                sb.Append(count);
+            }
+            return sb.ToString();
+        }
+    }
+}
