d7235c1a3504672fcf51c6f2437f1d8690a47d59
dich3v <radoslav.dichev@abv.bg>
2025-04-10 09:47:14 +0300

    Remove elements around bomb positions and calculate the sum

---
+++ CSharp-Learning-Project/Task02_Bomb/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task02_Bomb
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int[] bombNums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int result = RemoveNumbers(numbers, bombNums);
+            Console.WriteLine(result);
+        }
+        public static int RemoveNumbers(List<int> nums, int[] bombNumbers)
+        {
+            int bomb = bombNumbers[0];
+            int bombPower = bombNumbers[1];
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == bomb)
+                {
+                    int startIndex = i - bombPower;
+                    int endIndex = i + bombPower;
+
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+                    if (endIndex >= nums.Count)
+                    {
+                        endIndex = nums.Count - 1;
+                    }
+                    nums.RemoveRange(startIndex, endIndex - startIndex + 1);
+                    i = startIndex - 1;
+                }
+            }
+            return nums.Sum();
+        }
+    }
+}
