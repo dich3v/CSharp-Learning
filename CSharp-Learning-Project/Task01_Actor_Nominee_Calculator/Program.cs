23f5f8cb1c6e23c12864a67f04b18edbd43cdd36
dich3v <radoslav.dichev@abv.bg>
2025-04-10 08:54:42 +0300

    Implement actor nomination logic with points calculation and validation

---
+++ CSharp-Learning-Project/Task01_Actor_Nominee_Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task01_Actor_Nominee_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double startPoints = double.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            double finalPoints = startPoints;
+            double pointsNeeded = 1250.5;
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double points = double.Parse(Console.ReadLine());
+
+                finalPoints += (name.Length * points) / 2;
+                if (finalPoints > pointsNeeded)
+                {
+                    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {finalPoints:f1}!");
+                    return;
+                }
+            }
+            Console.WriteLine($"Sorry, {actorName} you need {pointsNeeded - finalPoints:f1} more!");
+        }
+    }
+}
