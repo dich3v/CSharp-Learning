bf0376e789d5bbb2494abb8b9735f0806479a0b6
dich3v <radoslav.dichev@abv.bg>
2025-04-10 11:23:59 +0300

    Track grades and stop after max poor grades, display average and problem count

---
+++ CSharp-Learning-Project/Task30_GradesAndProblemsCounter/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task30_GradesAndProblemsCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int negativeGrades = int.Parse(Console.ReadLine());
+
+            double grades = 0;
+            int problemCount = 0;
+            int poorGrades = 0;
+            string lastName = "";
+
+            string name = Console.ReadLine();
+            while (name != "Enough")
+            {
+                int grade = int.Parse(Console.ReadLine());
+                if (grade <= 4)
+                {
+                    poorGrades++;
+                    if (poorGrades == negativeGrades)
+                    {
+                        Console.WriteLine($"You need a break, {poorGrades} poor grades.");
+                        return;
+                    }
+                }
+                problemCount++;
+                grades += grade;
+                lastName = name;
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"Average score: {grades / problemCount}");
+            Console.WriteLine($"Number of problems: {problemCount}");
+            Console.WriteLine($"Last problem: {lastName}");
+        }
+    }
+}
