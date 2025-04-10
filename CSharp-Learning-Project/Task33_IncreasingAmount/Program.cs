c638428079cd60550f7cec4bd71c92d109532912
dich3v <radoslav.dichev@abv.bg>
2025-04-10 11:30:52 +0300

    Track and accumulate positive amounts, handle invalid operations and display total sum

---
+++ CSharp-Learning-Project/Task33_IncreasingAmount/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task33_IncreasingAmount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
