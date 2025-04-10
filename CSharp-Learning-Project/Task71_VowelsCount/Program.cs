c33ae6b7ad8f03b8ca3339fddac80a153b501610
dich3v <radoslav.dichev@abv.bg>
2025-04-10 13:55:49 +0300

    Implement vowel counter function

---
+++ CSharp-Learning-Project/Task71_VowelsCount/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task71_VowelsCount
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string result = VowelsCount(input);
+
+            Console.WriteLine(result);
+        }
+        public static string VowelsCount(string vowels)
+        {
+            int count = 0;
+
+            string vowelsToLower = vowels.ToLower();
+
+            for (int i = 0; i < vowelsToLower.Length; i++)
+            {
+                if (vowelsToLower[i] == 'a' || vowelsToLower[i] == 'e' || vowelsToLower[i] == 'o' || vowelsToLower[i] == 'u' || vowelsToLower[i] == 'i')
+                {
+                    count++;
+                }
+            }
+            return count.ToString();
+        }
+    }
+}
