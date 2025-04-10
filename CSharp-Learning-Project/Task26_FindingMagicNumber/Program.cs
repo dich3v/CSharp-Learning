9aaa380be791b43800565509fb58f6d43ccf6e90
dich3v <radoslav.dichev@abv.bg>
2025-04-10 11:11:59 +0300

    Add logic to find and count combinations of numbers summing to a magic number in a given range

---
+++ CSharp-Learning-Project/Task26_FindingMagicNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task26_FindingMagicNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+
+            int combinationCount = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    combinationCount++;
+                    if (magicNum == i + j)
+                    {
+                        Console.WriteLine($"Combination: {combinationCount}");
+                        Console.WriteLine($"{i} + {j} = {magicNum}");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{combinationCount} combinations - neither equals {magicNum}");
+        }
+    }
+}
