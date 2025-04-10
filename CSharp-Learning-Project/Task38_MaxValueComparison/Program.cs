9d451def29bae7cec2e9721491bed5ed6c1b25f9
dich3v <radoslav.dichev@abv.bg>
2025-04-10 11:46:44 +0300

    Max value comparison for different types

---
+++ CSharp-Learning-Project/Task38_MaxValueComparison/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task38_MaxValueComparison
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string output = GetMax(input);
+            Console.WriteLine(output);
+        }
+        public static string GetMax(string type)
+        {
+            if (type == "string")
+            {
+                string text1 = Console.ReadLine();
+                string text2 = Console.ReadLine();
+
+                return text1.CompareTo(text2) > 0 ? text1 : text2;
+            }
+            else if (type == "int")
+            {
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+
+                return num1 > num2 ? num1.ToString() : num2.ToString();
+            }
+            else
+            {
+                char char1 = char.Parse(Console.ReadLine());
+                char char2 = char.Parse(Console.ReadLine());
+
+                return char1 > char2 ? char1.ToString() : char2.ToString();
+            }
+        }
+    }
+}
