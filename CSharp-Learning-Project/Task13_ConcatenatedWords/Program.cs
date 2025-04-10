175734c21e19f3b66622a2df99c5c3f77b57da37
dich3v <radoslav.dichev@abv.bg>
2025-04-10 10:16:13 +0300

    Implemented word concatenation based on length

---
+++ CSharp-Learning-Project/Task13_ConcatenatedWords/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Task13_ConcatenatedWords
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+
+            string result = ConcatenateString(input).ToString();
+
+            Console.WriteLine(result);
+        }
+        public static StringBuilder ConcatenateString(string[] input)
+        {
+            StringBuilder concantenatedString = new StringBuilder();
+
+            foreach (string word in input)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    concantenatedString.Append(word);
+                }
+            }
+            return concantenatedString;
+        }
+    }
+}
