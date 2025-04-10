a5ab0eae2f42c18a7bea29a666cf99d4d1618272
dich3v <radoslav.dichev@abv.bg>
2025-04-10 13:47:08 +0300

    Implement function to replace banned words with asterisks in input text

---
+++ CSharp-Learning-Project/Task66_TextFilter/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task66_TextFilter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            string result = ReplaceBannedWords(text, bannedWords);
+            Console.WriteLine(result);
+        }
+        public static string ReplaceBannedWords(string text, string[] bannedWords)
+        {
+            foreach (string word in bannedWords)
+            {
+                string replacedWord = new string('*', word.Length);
+                text = text.Replace(word, replacedWord);
+            }
+            return text;
+        }
+    }
+}
