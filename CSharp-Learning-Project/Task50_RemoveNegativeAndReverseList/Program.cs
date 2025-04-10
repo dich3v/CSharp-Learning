399dc03a3286c9e96a45ff25a7b1e4431730e231
dich3v <radoslav.dichev@abv.bg>
2025-04-10 12:42:23 +0300

    Remove all negative numbers from the list and reverse the remaining elements

---
+++ CSharp-Learning-Project/Task50_RemoveNegativeAndReverseList/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task50_RemoveNegativeAndReverseList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] < 0)
+                {
+                    list.Remove(list[i]);
+                    i--;
+                }
+            }
+            list.Reverse();
+            if (list.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", list));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
