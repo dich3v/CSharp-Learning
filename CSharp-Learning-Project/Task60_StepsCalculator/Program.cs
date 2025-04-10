16b43b49e84feed3e86dde2c7bf722dae8f0ac36
dich3v <radoslav.dichev@abv.bg>
2025-04-10 13:32:33 +0300

    Track steps to reach 10,000 or handle 'Going home' input

---
+++ CSharp-Learning-Project/Task60_StepsCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task60_StepsCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int finalSteps = 0;
+            int goal = 10000;
+
+            while (finalSteps < goal)
+            {
+                string steps = Console.ReadLine();
+                if (steps == "Going home")
+                {
+                    int stepsToHome = int.Parse(Console.ReadLine());
+                    finalSteps += stepsToHome;
+                    break;
+                }
+                finalSteps += int.Parse(steps);
+            }
+            if (finalSteps >= goal)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{finalSteps - goal} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{goal - finalSteps} more steps to reach goal.");
+            }
+        }
+    }
+}
