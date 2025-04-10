1d0c5891daf96a554aafbb026219f35d0828c1e2
dich3v <radoslav.dichev@abv.bg>
2025-04-10 11:50:01 +0300

    Merge two lists by alternating elements from each list

---
+++ CSharp-Learning-Project/Task40_MergingLists/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task40_MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> secondList = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            List<int> mergedList = new List<int>();
+
+            int iterations = firstList.Count >= secondList.Count ? firstList.Count : secondList.Count;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                if (i < firstList.Count)
+                {
+                    mergedList.Add(firstList[i]);
+                }
+                if (i < secondList.Count)
+                {
+                    mergedList.Add(secondList[i]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", mergedList));
+        }
+    }
+}
