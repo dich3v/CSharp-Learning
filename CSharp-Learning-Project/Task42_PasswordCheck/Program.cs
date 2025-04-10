91ced8d794070bf9157654fd5a68b71405171920
dich3v <radoslav.dichev@abv.bg>
2025-04-10 12:29:19 +0300

    Implement password verification loop to welcome user upon correct password entry

---
+++ CSharp-Learning-Project/Task42_PasswordCheck/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task42_PasswordCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (password == input)
+                {
+                    Console.WriteLine($"Welcome {username}");
+                    break;
+                }
+            }
+        }
+    }
+}
