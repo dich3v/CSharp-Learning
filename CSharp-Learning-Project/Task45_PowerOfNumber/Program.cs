7de9c8b5bf7946e48fa2dd559feef787d9f1fe12
dich3v <radoslav.dichev@abv.bg>
2025-04-10 12:34:11 +0300

    Integer power calculation

---
+++ CSharp-Learning-Project/Task45_PowerOfNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task45_PowerOfNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int pow = int.Parse(Console.ReadLine());
+
+            int result = MathPow(number, pow);
+
+            Console.WriteLine(result);
+        }
+        static int MathPow(int num, int power)
+        {
+            int result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= num;
+            }
+            return result;
+        }
+    }
+}
