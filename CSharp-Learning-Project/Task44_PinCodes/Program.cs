fb4f2d96ee899fef1b7afad2e2d82109b3ae52ef
dich3v <radoslav.dichev@abv.bg>
2025-04-10 12:33:04 +0300

    Implemented nested loops with prime number check for generating specific combinations

---
+++ CSharp-Learning-Project/Task44_PinCodes/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task44_PinCodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= a; i++)
+            {
+                for (int j = 1; j <= b; j++)
+                {
+                    for (int k = 1; k <= c; k++)
+                    {
+                        if (i % 2 == 0 && k % 2 == 0)
+                        {
+                            if (IsPrime(j))
+                            {
+                                Console.WriteLine($"{i}{j}{k} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
