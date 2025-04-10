7cb8baa6a9d7d7d3cc47966ccdb71e1fe2eb903e
dich3v <radoslav.dichev@abv.bg>
2025-04-10 13:57:55 +0300

    Add functionality to reverse and display words until 'end' is entered

---
+++ CSharp-Learning-Project/Task72_WordReversal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Task72_WordReversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "";
+
+            while ((word = Console.ReadLine()) != "end")
+            {
+                StringBuilder reversedWord = new StringBuilder();
+
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reversedWord.Append(word[i]);
+                }
+                Console.WriteLine($"{word} = {reversedWord}");
+            }
+        }
+    }
+}
