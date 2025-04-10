046ad3595eca8de8c66f0f47c119dcf7f234f24f
dich3v <radoslav.dichev@abv.bg>
2025-04-10 12:47:19 +0300

    Implemented a dictionary-based resource tracker that accumulates quantities

---
+++ CSharp-Learning-Project/Task53_ResourcesQuantity/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task53_ResourcesQuantity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resource = "";
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            while ((resource = Console.ReadLine()) != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (dict.ContainsKey(resource))
+                {
+                    dict[resource] += quantity;
+                }
+                else
+                {
+                    dict.Add(resource, quantity);
+                }
+            }
+            foreach (KeyValuePair<string, int> kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
