0c5dbfdd743adfe834543aab7893c960a28d078d
dich3v <radoslav.dichev@abv.bg>
2025-04-10 13:19:33 +0300

    Added array rotation logic for shifting elements to the right by n positions

---
+++ CSharp-Learning-Project/Task55_ShiftElementsInArray/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task55_ShiftElementsInArray
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] rotatedNumbers = RotateArray(numbers, n);
+
+            Console.WriteLine(string.Join(" ", rotatedNumbers));
+        }
+        public static int[] RotateArray(int[] inputArray, int positions)
+        {
+            int[] resultArray = new int[inputArray.Length];
+            int length = inputArray.Length;
+            for (int i = 0; i < length; i++)
+            {
+                int newIndex = (i + positions) % length;
+                resultArray[newIndex] = inputArray[i];
+            }
+            return resultArray;
+        }
+    }
+}
