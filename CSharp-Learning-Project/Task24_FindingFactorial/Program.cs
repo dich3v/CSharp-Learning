d799afeb949091c1b5a4aefa211b4dc01c2ae171
dich3v <radoslav.dichev@abv.bg>
2025-04-10 11:00:56 +0300

    Implemented factorial division

---
+++ CSharp-Learning-Project/Task24_FindingFactorial/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task24_FindingFactorial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = FindFactorial(num1) / FindFactorial(num2);
+
+            Console.WriteLine(result);
+        }
+        public static int FindFactorial(int num)
+        {
+            int result = 1;
+
+            for (int i = 2; i <= num; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
