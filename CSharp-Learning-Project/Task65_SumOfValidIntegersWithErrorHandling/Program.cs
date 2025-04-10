a309d4bdd02923ca2d5de17bfe9913762aed421b
dich3v <radoslav.dichev@abv.bg>
2025-04-10 13:43:24 +0300

    Handle exceptions for invalid and out-of-range values

---
+++ CSharp-Learning-Project/Task65_SumOfValidIntegersWithErrorHandling/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task65_SumOfValidIntegersWithErrorHandling
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int sum = ProcessInputAndCalculateSum(input);
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+        public static int ProcessInputAndCalculateSum(string[] input)
+        {
+            int sum = 0;
+
+            foreach (string element in input)
+            {
+                try
+                {
+                    int number = int.Parse(element);
+                    sum += number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{element}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{element}' is out of range!");
+                }
+                Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+            }
+            return sum;
+        }
+    }
+}
