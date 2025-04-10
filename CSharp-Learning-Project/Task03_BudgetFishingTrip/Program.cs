935da7463fb437573f2d0fed41fde884146de070
dich3v <radoslav.dichev@abv.bg>
2025-04-10 09:50:44 +0300

    Add logic for calculation fishing trip cost based on season and group size

---
+++ CSharp-Learning-Project/Task03_BudgetFishingTrip/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Task03_BudgetFishingTrip
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermen = int.Parse(Console.ReadLine());
+            string result = CalculatingTrip(budget, season, fishermen);
+
+            Console.WriteLine(result);
+        }
+        public static string CalculatingTrip(int budget, string season, int fishermen)
+        {
+            int springRent = 3000;
+            int summerAutumn = 4200;
+            int winterRent = 2600;
+            double rent = 0;
+            double discount = 0;
+
+            if (season != "Summer" && season != "Autumn" && season != "Spring" && season != "Winter")
+            {
+                return "Invalid season!";
+            }
+
+            if (fishermen <= 6 && fishermen > 0)
+            {
+                discount = 0.9;
+            }
+            else if (fishermen >= 7 && fishermen <= 11)
+            {
+                discount = 0.85;
+            }
+            else if (fishermen >= 12)
+            {
+                discount = 0.75;
+            }
+            switch (season)
+            {
+                case "Summer":
+                    rent = summerAutumn * discount; break;
+                case "Autumn":
+                    rent = summerAutumn * discount; break;
+                case "Spring":
+                    rent = springRent * discount; break;
+                case "Winter":
+                    rent = winterRent * discount; break;
+                default: break;
+            }
+            if (fishermen % 2 == 0 && season != "Autumn")
+            {
+                rent *= 0.95;
+            }
+            double finalSum = budget - rent;
+            if (finalSum >= 0)
+            {
+                return $"Yes! You have {finalSum:f2} leva left.";
+            }
+            else
+            {
+                return $"Not enough money! You need {Math.Abs(finalSum):f2} leva.";
+            }
+        }
+    }
+}
