66b0d352bde0cffdbb98c85c952b870db8402b55
dich3v <radoslav.dichev@abv.bg>
2025-04-18 15:15:24 +0300

    Add logic to check if sum of numbers excluding max equals max number

--- CSharp-Learning-Project/Task37_MaxNumberSumComparison/Program.cs
+++ CSharp-Learning-Project/Task37_MaxNumberSumComparison/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+
+            int maxNum = int.MinValue;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (num > maxNum)
+                {
+                    maxNum = num;
+                }
+            }
+            int finalSum = sum - maxNum;
+            if (finalSum == maxNum)
+            {
+                Console.WriteLine($"Yes sum = {finalSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No diff is {Math.Abs(finalSum - maxNum)}");
+            }
         }
     }
 }
