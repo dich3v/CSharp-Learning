8f524eb075af398e5600319348cdfc125d5ec3cd
dich3v <radoslav.dichev@abv.bg>
2025-04-18 15:12:58 +0300

    Track and accumulate positive amounts, handle invalid operations and display total sum

--- CSharp-Learning-Project/Task33_IncreasingAmount/Program.cs
+++ CSharp-Learning-Project/Task33_IncreasingAmount/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+            double finalSum = 0;
+
+            while (input != "NoMoreMoney")
+            {
+                double sum = double.Parse(input);
+                if (sum < 0)
+                {
+                    Console.WriteLine("Invalid Operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {sum:f2}");
+                finalSum += sum;
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {finalSum:F2}");
         }
     }
 }
