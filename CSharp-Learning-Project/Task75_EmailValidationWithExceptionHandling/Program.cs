8e52c18068ae63e9adb103795d34b548f6845179
dich3v <radoslav.dichev@abv.bg>
2025-04-19 14:19:41 +0300

    Implement email validation with regex and error handling

--- CSharp-Learning-Project/Task75_EmailValidationWithExceptionHandling/Program.cs
+++ CSharp-Learning-Project/Task75_EmailValidationWithExceptionHandling/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Task75_EmailValidationWithExceptionHandling
+﻿using System.Text.RegularExpressions;
+
+namespace Task75_EmailValidationWithExceptionHandling
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string email = Console.ReadLine();
+
+            Regex validEmail = new Regex(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(email) || email.Contains(' '))
+                {
+                    throw new ArgumentException("Invalid email: Email cannot be empty or contain spaces");
+                }
+                if (email.StartsWith("@") || email.EndsWith("."))
+                {
+                    throw new FormatException("Invalid email: Cannot start with '@' or end with '.'");
+                }
+                if (!validEmail.IsMatch(email))
+                {
+                    throw new FormatException("Invalid email format.");
+                }
+                Console.WriteLine("Valid email");
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
         }
     }
 }
