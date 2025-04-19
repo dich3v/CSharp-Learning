a7072a4d24a98c6ca3e9e3d483f7eff92050188c
dich3v <radoslav.dichev@abv.bg>
2025-04-19 14:17:02 +0300

    Add basic deposit and withdrawal processor with error handling

---
+++ CSharp-Learning-Project/Task77_BankTransactionProcessor/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Task77_BankTransactionProcessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            int finalSum = 0;
+
+            while (command != "end")
+            {
+                int money = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    if (money < 0)
+                    {
+                        throw new ArgumentException("Negative deposit or withdrawal");
+                    }
+                    if (command == "deposit")
+                    {
+                        finalSum += money;
+                    }
+                    else if (command == "withdraw")
+                    {
+                        finalSum -= money;
+                        if (finalSum < 0)
+                        {
+                            throw new Exception("Insufficient Funds");
+                        }
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input");
+                    continue;
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc.Message);
+                    return;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(finalSum);
+        }
+    }
+}
