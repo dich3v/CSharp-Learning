6911b2edd66767eb5da7af8199d0521e556c6982
dich3v <radoslav.dichev@abv.bg>
2025-04-25 13:09:05 +0300

    Implement logic to merge consecutive duplicate values by summing them

---
+++ CSharp-Learning-Project/Task78_AdjecentEqual/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task78_AdjecentEqual
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            string result = Sum(list);
+
+            Console.WriteLine(result);
+        }
+        public static string Sum(List<int> numbers)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i == 0 || numbers[i] != numbers[i - 1])
+                {
+                    result.Add(numbers[i]);
+                    continue;
+                }
+                result[result.Count - 1] += numbers[i];
+            }
+
+            return string.Join(" ", result);
+        }
+    }
+}
