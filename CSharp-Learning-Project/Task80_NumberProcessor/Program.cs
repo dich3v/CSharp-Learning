cf2e925658e48243530ec32b0fd43fd0d61e9d21
dich3v <radoslav.dichev@abv.bg>
2025-04-28 14:10:01 +0300

    Implement number processing: power for evens, sqrt for odds

---
+++ CSharp-Learning-Project/Task80_NumberProcessor/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task80_NumberProcessor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            List<double> result = NumberProcessor(list);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+        public static List<double> NumberProcessor(List<int> numbers)
+        {
+            List<double> result = new List<double>();
+
+            foreach (int number in numbers)
+            {
+                if (number < 0)
+                {
+                    result.Add(number);
+                }
+                else if (number % 2 == 0)
+                {
+                    result.Add(Math.Pow(number, 2));
+                }
+                else
+                {
+                    result.Add(Math.Sqrt(number));
+                }
+            }
+            return result;
+        }
+    }
+}
