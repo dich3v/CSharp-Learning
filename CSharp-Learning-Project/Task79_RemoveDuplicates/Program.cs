62a04739499a2d14d87c093dd50ac78f1879a736
dich3v <radoslav.dichev@abv.bg>
2025-04-28 14:07:40 +0300

    Implement remove duplicates and return sorted unique numbers

---
+++ CSharp-Learning-Project/Task79_RemoveDuplicates/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task79_RemoveDuplicates
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] result = RemoveDuplicates(nums);
+
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+        public static int[] RemoveDuplicates(int[] numbers)
+        {
+            HashSet<int> uniqueNumbers = new();
+
+            foreach (int number in numbers)
+            {
+                uniqueNumbers.Add(number);
+            }
+
+            return uniqueNumbers.ToArray();
+        }
+    }
+}
