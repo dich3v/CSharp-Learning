c7e8e8f2502fc00e591fb010391f52e57bc535fd
dich3v <radoslav.dichev@abv.bg>
2025-05-05 13:33:57 +0300

    Add character counting from list of strings

---
+++ CSharp-Learning-Project/Task82_CharacterCounter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Task82_CharacterCounter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split().ToList();
+            string result = Count(list);
+
+            Console.WriteLine(result);
+        }
+        public static string Count(List<string> list)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach (string item in list)
+            {
+                foreach (char c in item)
+                {
+                    if (!dict.ContainsKey(c))
+                    {
+                        dict[c] = 1;
+                    }
+                    else
+                    {
+                        dict[c]++;
+                    }
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (KeyValuePair<char, int> kvp in dict)
+            {
+                sb.AppendLine($"{kvp.Key} -> {kvp.Value}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
